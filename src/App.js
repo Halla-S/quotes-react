97b8cdb513186f83d4efc31659ea133f94b11c5d
Halla Sulaiman <86550752+Halla-S@users.noreply.github.com>
2022-04-05 03:34:05 +0100

    Fixing the rendering problem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import Quotes from "./quotes";
 import "./App.css";
 const App = (props) => {
-  const [quotes, setQuotes] = useState([]);
-
+  const [quotes, setQuotes] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
   const getQuotes = () => {
+    setIsLoading(true);
     fetch(`https://halla-quote-server.glitch.me/quotes/random`)
       .then(function (response) {
         if (response.ok) {
@@ -19,17 +19,29 @@ const App = (props) => {
       .catch(function (err) {
         console.log(err.message);
       });
+      setIsLoading(false);
   };
   useEffect(() => {
     getQuotes();
   }, []);
+  console.log(quotes);
+  if(Object.keys(quotes).length >0){ 
+    return (
+      <div>
+        <h1>quote:{quotes.quote}</h1>
+        <h4>"Author:{quotes.author}"</h4>
+        <button onClick={() => getQuotes()}>Get a random quote</button>
+      </div>
+    );
+        
 
-  return (
-    <div>
-      <Quotes quote={quotes.quote} author={quotes.author} />
-      <button onClick={() => getQuotes()}>Get a random quote</button>
-    </div>
-  );
+    
+  }
+  else {
+    return <h1>{isLoading}</h1>;
+    
+  }
+ 
 };
 
 export default App;
